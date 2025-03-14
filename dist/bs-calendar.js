19356e6865051e0219bc9b029236d1843fad7d4d
Thomas Kirsch <t.kirsch@webcito.de>
2025-03-14 11:32:40 +0100

    Remove redundant padding class in modal-body element
    Removed an unnecessary `px-4` class from the modal-body element to ensure consistent styling and reduce unwanted padding. This update simplifies the layout while preserving the intended appearance.

--- dist/bs-calendar.js
+++ dist/bs-calendar.js
@@ -3854,7 +3854,7 @@
                     `<div class="modal-content border border-1 shadow" style="${roundedCss}">`,
                     `<div class="modal-body d-flex flex-column align-items-stretch pb-4" style="">`,
                     `<div class="d-flex justify-content-end align-items-center" data-modal-options>`,
-                    `<button type="button" data-dismiss="modal" data-bs-dismiss="modal" class="btn"><i class="bi bi-x-lg"></i> </button>`,
+                    `<button type="button" data-dismiss="modal" data-bs-dismiss="modal" class="btn p-0"><i class="bi bi-x-lg"></i></button>`,
                     `</div>`,
                     `<div class="modal-appointment-content flex-fill overflow-y-auto" style="">`,
                     html,
